ad3fed3388c61ff697ba8f80beded44212425683
gaellegoyon <gaellegoyon.pro@gmail.com>
2023-01-12 16:31:44 +0100

    add reserv

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import CarFilter from "@components/CarFilter";
 import BikeFilter from "@components/BikeFilter";
 import ScooterFilter from "@components/ScooterFilter";
-import Rayon from "@components/Rayon";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import CardVehicle from "../components/CardVehicle";
@@ -17,15 +16,8 @@ function Home() {
           <ScooterFilter />
           <BikeFilter />
         </div>
-        <div className=" flex justify-center item-center h-[7vh]">
-          <div className="flex flex-col justify-center items-center">
-            <div className="text-white font-main-font pt-12">Rayon :</div>
-            <div className="w-full">
-              <Rayon />
-            </div>
-          </div>
-        </div>
-        <div className="overflow-auto h-[70vh]">
+
+        <div className="overflow-auto h-[80vh]">
           <CardVehicle />
           <CardVehicle />
           <CardVehicle />
